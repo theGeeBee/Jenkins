0ff52fa7013edd808277d4a65de1c35b67b7bef3
Gibran Khan <gibran.khan@atos.net>
2023-09-26 13:45:10 +0200

    fixed main function

--- Sample.java
+++ Sample.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Sample {
+public class Sample {
+
+    public static void main(String[] args)  {
+        System.out.println("Welcome to my Jenkins project!");
+    }
+
 }
